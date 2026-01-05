31a069da456ad509a577e01f9c72c2995d6365e7
M Manjunatha <99828070+M-Manjunatha@users.noreply.github.com>
2026-01-05 22:42:41 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (5.82%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,14 +1,30 @@
+// class Solution {
+// public:
+//     bool isPalindrome(int x) {
+//         if(x<0) return false;
+
+//         long rev=0, num=x;
+//         while(num){
+//             rev=rev*10+num%10;
+//             num=num/10;
+//         }
+//         return rev==x;
+        
+//     }
+// };
+
+//Another Method that can convert from std::string
+#include <algorithm>
+#include<string>
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x<0) return false;
-
-        long rev=0, num=x;
-        while(num){
-            rev=rev*10+num%10;
-            num=num/10;
-        }
-        return rev==x;
+       std::string s=std::to_string(x); //convert int to string
+       std::reverse(s.begin(),s.end()); //reverse the string
+       int y=std::stoll(s);
+       if(y==x){
+        return true;
+       }else return false;
         
     }
 };
