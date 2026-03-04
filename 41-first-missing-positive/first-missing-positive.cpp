b5d18d1dc9d65f6f9746d055ee4595aa5263ab96
M Manjunatha <99828070+M-Manjunatha@users.noreply.github.com>
2026-03-04 22:59:59 +0530

    Time: 21 ms (21.07%) | Memory: 54.9 MB (85.13%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
+       // Step1: Remove Negative numbers and zero
         nums.erase(remove_if(nums.begin(),nums.end(),[](int n) {return n<=0;}),nums.end());
         sort(nums.begin(),nums.end());
+
+        //Step 2: Compare the target with elements.
         int target=1;
         for(int i : nums){
             if(i==target){
